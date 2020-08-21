427d5e24e6e1660963b8aabe4965b948359a0b52
jack.wu <jack.wu@estonetech.com>
2020-08-21 14:05:22 +0800

    add imx8mq imx-mkimage support

---
+++ make.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "build uboot-kernel"
+
+source /mnt/ben/work_imx8/kernel-uboot/4.14.98_2.0.0_GA/sdk/install/environment-setup-aarch64-poky-linux
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-poky-linux-
+
+export export LDFLAGS=""
+
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-gnu-
+cp -vt iMX8M/ ../uboot-imx/spl/u-boot-spl.bin \
+	../uboot-imx/u-boot-nodtb.bin \
+	../uboot-imx/u-boot.bin \
+	../uboot-imx/arch/arm/dts/fsl-imx8mq-evk.dtb
+cp -v ../uboot-imx/tools/mkimage iMX8M/mkimage_uboot
+make clean
+make SOC=iMX8M DTBS=fsl-imx8mq-evk.dtb flash_hdmi_spl_uboot
+
+cd iMX8M && ./print_fit_hab.sh 0x60000 fsl-imx8mq-evk.dtb ; cd ..
+echo "Next: dd if=iMX8M/flash.bin of=/dev/sd[x] bs=1K seek=33 skip=0"
+
+#cp iMX8M/flash.bin /mnt/ben/work_imx8/kernel-uboot/4.14.98_2.0.0_GA/emb-2238-mfg/mfgtools/Profiles/Linux/OS\ Firmware/files/linux/ 

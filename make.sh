c8d4748640210e27be2e41b9aa1ed997c28abab7
jack.wu <jack.wu@estonetech.com>
2021-09-01 18:01:49 +0800

    add poe2237-tas support

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@ export CROSS_COMPILE=aarch64-linux-gnu-
 ############build uboot-imx#####################
 cd ../uboot-imx
 make distclean
-make imx8mm_iob4607_defconfig
+make imx8mm_poe2237_defconfig 
 make -j8
 cd -
 ############build flash.bin#####################
@@ -15,22 +15,22 @@ cp -vt iMX8M/ ../uboot-imx/spl/u-boot-spl.bin \
 	../uboot-imx/u-boot.bin \
 	../uboot-imx/arch/arm/dts/est-imx8mm-iob4607.dtb
 cp -v ../uboot-imx/tools/mkimage iMX8M/mkimage_uboot
-mv iMX8M/est-imx8mm-iob4607.dtb iMX8M/fsl-imx8mm-evk.dtb
+mv iMX8M/est-imx8mm-poe2237.dtb iMX8M/fsl-imx8mm-evk.dtb
 make clean
 make SOC=iMX8MM flash_evk
 echo "Next: dd if=iMX8M/flash.bin of=/dev/sd[x] bs=1K seek=33 skip=0"
 ############build linux-imx#####################
 cd ../linux-imx
 make distclean
-make iob4607_defconfig
+make poe2237_defconfig
 make -j8
 ############build uuu dir#####################
 cd -
 cp iMX8M/flash.bin ./uuu/
 cp ../linux-imx/arch/arm64/boot/Image ./uuu/
-cp ../linux-imx/arch/arm64/boot/dts/freescale/est-imx8mm-iob4607-lvds-7.dtb ./uuu/
+cp ../linux-imx/arch/arm64/boot/dts/freescale/est-imx8mm-poe2237-tas.dtb ./uuu/
 cd uuu
-zip -r ../iob4607-uuu.zip ./*
+zip -r ../poe2237-tas-uuu.zip ./*
 cd -
 
 

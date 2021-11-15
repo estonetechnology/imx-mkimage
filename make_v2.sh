5bb3072247aa82ed3b0e8c4af59b1b0f5ca90020
jack.wu <jack.wu@estonetech.com>
2021-11-15 15:10:46 +0800

    add tas v2 vesion support

---
+++ make_v2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "build uboot-kernel"
+export export LDFLAGS=""
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-gnu-
+############build uboot-imx#####################
+cd ../uboot-imx
+make distclean
+make imx8mm_poe2237_v2_defconfig 
+make -j8
+cd -
+############build flash.bin#####################
+cp -vt iMX8M/ ../uboot-imx/spl/u-boot-spl.bin \
+	../uboot-imx/u-boot-nodtb.bin \
+	../uboot-imx/u-boot.bin \
+	../uboot-imx/arch/arm/dts/est-imx8mm-poe2237-v2.dtb
+cp -v ../uboot-imx/tools/mkimage iMX8M/mkimage_uboot
+mv iMX8M/est-imx8mm-poe2237-v2.dtb iMX8M/fsl-imx8mm-evk.dtb
+make clean
+make SOC=iMX8MM flash_evk
+echo "Next: dd if=iMX8M/flash.bin of=/dev/sd[x] bs=1K seek=33 skip=0"
+############build linux-imx#####################
+cd ../linux-imx
+make distclean
+make poe2237_defconfig
+make -j8
+############build uuu dir#####################
+cd -
+cp iMX8M/flash.bin ./uuu/
+cp ../linux-imx/arch/arm64/boot/Image ./uuu/
+cp ../linux-imx/arch/arm64/boot/dts/freescale/est-imx8mm-poe2237-tas-v2-m4.dtb ./uuu/est-imx8mm-poe2237-tas.dtb
+cd uuu
+zip -r ../iob-2237-tas-v2-uuu.zip ./*
+cd -
+
+
